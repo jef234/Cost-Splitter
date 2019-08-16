e5146bf56305e54367ca6dd3099dde7f05f3f34c
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-16 10:59:54 +0530

    Auto-generated commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    function compute() {
+        let txtTotalBill = document.querySelector("#txtTotalBill"),
+            txtTipInPercentage = document.querySelector("#txtTipInPercentage"),
+            txtNumberOfPeople = document.querySelector("#txtNumberOfPeople"),
+            txtTipPerPerson = document.querySelector("#txtTipPerPerson"),
+            txtTotalPerPerson = document.querySelector("#txtTotalPerPerson");
+        if (txtTotalBill.value != "" && txtTipInPercentage.value != "" && txtNumberOfPeople.value != "") {
+            txtTipPerPerson.innerHTML = ((+txtTotalBill.value / +txtTipInPercentage.value) / +txtNumberOfPeople.value);
+            txtTotalPerPerson.innerHTML = ((+txtTotalBill.value / +txtNumberOfPeople.value) + +txtTipPerPerson.innerHTML) + "&#8377";
+            txtTipPerPerson.innerHTML += "&#8377";
+        } else {
+            txtTipPerPerson.value = "";
+            txtTotalPerPerson.value = "";
+        }
+    }
+
+    $("#txtTotalBill").bind('input', compute);
+    $("#txtTipInPercentage").bind('input', compute);
+    $("#txtNumberOfPeople").bind('input', compute);
+});
